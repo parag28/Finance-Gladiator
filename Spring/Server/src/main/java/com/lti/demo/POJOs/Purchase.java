9345427d008106dafd73743443d9615c24b99e2c
manojjanapala <manojjanapala18@gmail.com>
2021-02-01 19:22:00 +0530

    added

---
+++ Spring/Server/src/main/java/com/lti/demo/POJOs/Purchase.java
@@ -0,0 +1,64 @@
+package com.lti.demo.DAOs;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+@Entity(name="Purchase")
+public class Purchase {
+
+	@Id
+	@Column(name="purchaseid")
+	private int purchaseid;
+	
+	@ManyToOne
+	@JoinColumn(name="username")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name="product")
+	private Product product;
+	
+	@Column(name="purchaseddate")
+	private Date purchaseddate;
+
+	public int getPurchaseid() {
+		return purchaseid;
+	}
+
+	public void setPurchaseid(int purchaseid) {
+		this.purchaseid = purchaseid;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Date getPurchaseddate() {
+		return purchaseddate;
+	}
+
+	public void setPurchaseddate(Date purchaseddate) {
+		this.purchaseddate = purchaseddate;
+	}
+}
+
+
