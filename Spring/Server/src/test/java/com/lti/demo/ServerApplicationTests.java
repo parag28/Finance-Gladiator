d4a4b034d81409b2b631870161afa8e9404b0409
manojjanapala <manojjanapala18@gmail.com>
2021-02-02 10:20:32 +0530

    codecommit

--- Spring/Server/src/test/java/com/lti/demo/ServerApplicationTests.java
+++ Spring/Server/src/test/java/com/lti/demo/ServerApplicationTests.java
@@ -5,13 +5,9 @@ package com.lti.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import com.lti.demo.POJOs.Product;
 import com.lti.demo.POJOs.Purchase;
 import com.lti.demo.POJOs.User;
-import com.lti.demo.Repositories.PurchasesRepository;
-import com.lti.demo.Repositories.PurchasesRepositoryImplementation;
-
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,25 +19,10 @@ import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 class ServerApplicationTests {
-	
-	@Autowired
-	PurchasesRepositoryImplementation pr;
-	
+		
     @Test
     void contextLoads() {
-    	Date date=new Date();
-		Purchase pur=new Purchase();
-		pur.setPurchaseid(1);
-		User u1=new User();
-		u1.setName("manoj");
-		Product p1=new Product();
-		p1.setProductName("pen");
-		pur.setUser(u1);
-		pur.setProduct(p1);
-		pur.setPurchaseddate(date);
-		
-		pr.addPurchase(pur);
-    }
+    	    }
     
    	
 
