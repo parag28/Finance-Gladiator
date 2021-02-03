e5af02c375f3811ba3c8cefdd3abd4f7eb1af0c0
parag28 <51826418+parag28@users.noreply.github.com>
2021-02-03 14:34:19 +0530

    Updated EMIController and ProductController

---
+++ Spring/Server/main/java/com/development/demo/layer4/EMIController.java
@@ -0,0 +1,70 @@
+package com.development.demo.layer4;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.development.demo.layer1.Emi;
+import com.development.demo.layer3.EmiService;
+
+@RestController
+@CrossOrigin
+public class EMIController {
+	
+	@Autowired
+	EmiService emiService;
+	@GetMapping(value="/getEMI/{EMIId}")
+	@ResponseBody
+	@CrossOrigin
+	public Emi getEMI(@PathVariable int EMIId)
+	{
+		try {
+			return emiService.getEMIService(EMIId);
+		}catch(Exception e) {
+			System.out.println("Exception");
+			return null;
+		}
+		
+	}
+	
+	@GetMapping("/getAllEMIs")
+	@ResponseBody
+	@CrossOrigin
+	public List<Emi>getAllEMIs(){
+		List<Emi> emiList= (List<Emi>)emiService.getAllEMIsService();
+		return emiList;
+	}
+	
+	@PostMapping(path="/addEMI")
+	@ResponseBody
+	@CrossOrigin
+	public String addEMI(@RequestBody Emi emi)
+	{
+		try {
+			emiService.addEMIService(emi);
+			return "success";
+		}catch(Exception e) {
+			return "fail";
+		}
+	}
+	
+	@PostMapping(path="/updateEMI")
+	@ResponseBody
+	@CrossOrigin
+	public String updateEMI(@RequestBody Emi emi)
+	{
+		try {
+			return emiService.updateEMIService(emi);
+		}catch(Exception e) {
+			return "fail";
+		}
+	}
+	
+}
