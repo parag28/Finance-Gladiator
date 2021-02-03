a12b9f293f33fb1ead2f51d14b190e5307222885
reddydavinci <busisatya@gmail.com>
2021-02-03 16:07:10 +0530

    App product list done

--- AngularInterfaces/myproject/src/app/app-routing.module.ts
+++ AngularInterfaces/myproject/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProductlistComponent} from './productlist/productlist.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    declarations: [
+        ProductlistComponent
+    ],
+    exports: [RouterModule, ProductlistComponent]
 })
 export class AppRoutingModule { }
