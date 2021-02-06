2d41503432dedac8dd0d518c7fa81799791c8b7c
bindu141298 <bindupallavi14@gmail.com>
2021-02-06 22:49:47 +0530

    For Admin

--- AngularInterfaces/myproject/src/app/adminpage/adminpage.component.ts
+++ AngularInterfaces/myproject/src/app/adminpage/adminpage.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ApproveuserService } from '../approveuser.service';
+import { Login } from '../login';
+import { LoginService } from '../login.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-adminpage',
-  templateUrl: './adminpage.component.html',
+  templateUrl:'./adminpage.component.html',
   styleUrls: ['./adminpage.component.css']
 })
 export class AdminpageComponent implements OnInit {
+ // userName: string |null = localStorage.getItem("localVariableUserName");
+ userName:string="bindu19";
+  private subscription: Subscription;
+  user: User;
+  UsersList: User[];
+  UsersListDisplay: User[][];
+  result:string;
+login:Login;
 
-  constructor() { }
+  constructor(private approveuserService: ApproveuserService,private loginService: LoginService, private router: Router ) { }
 
   ngOnInit(): void {
+    this.subscription = this.approveuserService.getUsersByUserName(this.userName).subscribe((data: User ) => {
+      this.user = data;
+      console.log("userlist checking");
+      console.log(this.user);
+      }, (err: any) => {
+       console.log(err);
+     });
   }
+
+  
+  function(){
+    let subscription= this.loginService.getLoginsByUserName(this.userName).subscribe((data: Login)=>{
+      this.login=data;
+      this.login.userapprovalstatus=1;
+      this.loginService.updatelogin(this.login).subscribe((data:string)=>{
+        this.result=data;
+      });
+    }, (err: any) => {
+      console.log(err);
+    
+    });
+      this.loginService.updatelogin(this.login).subscribe((data:string)=>{
+        this.result=data;
+      }, (err: any) => {
+        console.log(err);
+    });
+  }
+
+
 
 }
--- AngularInterfaces/myproject/src/app/login.service.ts
+++ AngularInterfaces/myproject/src/app/login.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Product} from './product';
 import {Login} from './login';
 
 @Injectable({
@@ -11,10 +10,17 @@ export class LoginService {
   baseUrl: string = 'http://localhost:8080/';
   login:Login;
   constructor(private http:HttpClient) { }
-  getLoginByUserName(username: string) :Observable<Login>{
+  getAllLogins() :Observable<Login[]>
+  { console.log("loginservice called");
+    return this.http.get<Login[]>(this.baseUrl+"getAllLogins/");
+  }
+
+  getLoginsByUserName(username: string) :Observable<Login>{
     console.log("Hit in service login: method getLoginByUserName");
     return this.http.get<Login>(this.baseUrl+"getLogin/"+username);
   }
-
-
+  updatelogin(login :Login) :Observable<string>{
+    console.log("Hit in updateLogin");
+    return this.http.post<string>(this.baseUrl+"updateLogin/",login);
+  }
 }
